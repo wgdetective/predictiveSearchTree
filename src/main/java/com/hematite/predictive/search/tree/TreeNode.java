9ae6193b5051c2a25132e7bc7a22cb43b3931445
dePelimesha <walik-1998@mail.ru>
2019-03-28 18:21:29 +0300

    Add tree generation

--- src/main/java/com/hematite/predictive/search/tree/TreeNode.java
+++ src/main/java/com/hematite/predictive/search/tree/TreeNode.java
@@ -2,19 +2,27 @@ package com.hematite.predictive.search.tree;
 
 import lombok.Data;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Data
 public class TreeNode {
     private final String key;
-    private final List<String> values;
+    private final List<NodeData<String>> values;
     private final Map<String, TreeNode> childNodes;
     private final TreeNode parentNode;
 
+    public TreeNode(final String key, final List<NodeData<String>> values) {
+        this(key, values, null);
+    }
+
+    public TreeNode(final String key, final List<NodeData<String>> values, final TreeNode parentNode) {
+        this(key, values, new HashMap<>(), parentNode);
+    }
 
     public TreeNode(final String key,
-                    final List<String> values,
+                    final List<NodeData<String>> values,
                     final Map<String, TreeNode> childNodes,
                     final TreeNode parentNode) {
         this.key = key;
@@ -23,7 +31,13 @@ public class TreeNode {
         this.parentNode = parentNode;
     }
 
+
+
     public void addChildNode(final String key, final TreeNode node) {
         childNodes.put(key, node);
+    }
+
+    public boolean hasChild() {
+        return !childNodes.isEmpty();
     }
 }
