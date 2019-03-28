3e38def4607d22f78cab533681cff24bcf16d6a9
dePelimesha <walik-1998@mail.ru>
2019-03-28 13:26:46 +0300

    issue#2 Create TreeNode structure

---
+++ src/main/java/com/wgdetective/predictive/tree/TreeNode.java
@@ -0,0 +1,29 @@
+package com.wgdetective.predictive.tree;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class TreeNode {
+    private final String key;
+    private final List<String> values;
+    private final Map<String, TreeNode> childNodes;
+    private final TreeNode parentNode;
+
+
+    public TreeNode(final String key,
+                    final List<String> values,
+                    final Map<String, TreeNode> childNodes,
+                    final TreeNode parentNode) {
+        this.key = key;
+        this.values = values;
+        this.childNodes = childNodes;
+        this.parentNode = parentNode;
+    }
+
+    public void addChildNode(final String key, final TreeNode node) {
+        childNodes.put(key, node);
+    }
+}
