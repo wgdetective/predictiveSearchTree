8ca253590b072edf4cdea3830eaf327993ad031f
MariaRozhkova <mary.wootsen.96@gmail.com>
2019-03-29 12:47:53 +0300

    srename SqlDataProvider

--- predictive-search-tree-db/src/main/java/com/hematite/predictive/search/dataprovider/SqlDataProvider.java
+++ predictive-search-tree-db/src/main/java/com/hematite/predictive/search/dataprovider/SqlDataProvider.java
@@ -4,7 +4,7 @@ import com.hematite.predictive.search.tree.NodeData;
 
 import java.util.List;
 
-public class MysqlDataProvider implements DataProvider {
+public class SqlDataProvider implements DataProvider {
 
     @Override
     public List<NodeData> getAllData() {
