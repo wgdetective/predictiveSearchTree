f1e508eeb08f7f3c4de22af11b6d47876ab2851c
MariaRozhkova <mary.wootsen.96@gmail.com>
2019-03-29 19:22:47 +0300

    fill db with values

--- predictive-search-tree-db/src/main/java/com/hematite/predictive/search/dataprovider/SqlDataProvider.java
+++ predictive-search-tree-db/src/main/java/com/hematite/predictive/search/dataprovider/SqlDataProvider.java
@@ -1,13 +1,19 @@
 package com.hematite.predictive.search.dataprovider;
 
 import com.hematite.predictive.search.tree.NodeData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class SqlDataProvider implements DataProvider {
+
+    @Autowired
+    private TreeNodeRepository treeNodeRepository;
 
     @Override
     public List<NodeData> getAllData() {
-        return null;
+        return treeNodeRepository.findAll();
     }
 }
--- predictive-search-tree-db/src/main/java/com/hematite/predictive/search/dataprovider/TreeNodeRepository.java
+++ predictive-search-tree-db/src/main/java/com/hematite/predictive/search/dataprovider/TreeNodeRepository.java
@@ -1,9 +1,10 @@
 package com.hematite.predictive.search.dataprovider;
 
+import com.hematite.predictive.search.tree.NodeData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//@Repository
-//public interface TreeNodeRepository extends JpaRepository<Object, Long> {
-//
-//}
+@Repository
+public interface TreeNodeRepository extends JpaRepository<NodeData, Long> {
+
+}
