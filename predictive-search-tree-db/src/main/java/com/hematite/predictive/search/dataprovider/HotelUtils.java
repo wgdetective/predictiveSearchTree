597e860c4e58bde5f7d81b2bed026719c193d082
dePelimesha <walik-1998@mail.ru>
2019-04-04 15:29:48 +0300

    refactor hotels util and liquibase

--- predictive-search-tree-db/src/main/java/com/hematite/predictive/search/dataprovider/HotelUtils.java
+++ predictive-search-tree-db/src/main/java/com/hematite/predictive/search/dataprovider/HotelUtils.java
@@ -9,14 +9,6 @@ import java.util.List;
 public class HotelUtils {
     public static void main(String[] args) {
         try {
-            final File newFile = new File("./predictive-search-tree-db/src/main/resources/sql/hotels.sql");
-            newFile.createNewFile();
-
-            final FileWriter fileWriter = new FileWriter(newFile, false);
-            fileWriter.write("CREATE PROCEDURE writeData()\n" +
-                    "BEGIN\n" +
-                    "INSERT INTO hotels(ids, star, name) values");
-
             final List<String> filesWithDefaultSeparator = new ArrayList<>();
             filesWithDefaultSeparator.add("./predictive-search-tree-db/src/main/resources/csvFilesNew/hotels.gta.2019-03-26T23_00_01.csv");
             filesWithDefaultSeparator.add("./predictive-search-tree-db/src/main/resources/csvFilesNew/hotels.hotelbeds.2019-03-26T23_00_01.csv");
@@ -25,44 +17,54 @@ public class HotelUtils {
             filesWithDefaultSeparator.add("./predictive-search-tree-db/src/main/resources/csvFilesNew/hotels.rooms.2019-03-26T23_00_01.csv");
             filesWithDefaultSeparator.add("./predictive-search-tree-db/src/main/resources/csvFilesNew/hotels.sunhotels.2019-03-26T23_00_01.csv");
             filesWithDefaultSeparator.add("./predictive-search-tree-db/src/main/resources/csvFilesNew/hotels.tourico.2019-03-26T23_00_01.csv");
-            filesWithDefaultSeparator.add("./predictive-search-tree-db/src/main/resources/csvFilesNew/hotels.tourico.2019-03-26T23_00_01New.csv");
             filesWithDefaultSeparator.add("./predictive-search-tree-db/src/main/resources/csvFilesNew/hotels.travco.2019-03-26T23_00_01.csv");
 
             final List<String> filesWithSpecialSeparator = new ArrayList<>();
             filesWithSpecialSeparator.add("./predictive-search-tree-db/src/main/resources/csvFilesNew/hotels.gp.2019-03-26T23_00_01.csv");
             filesWithSpecialSeparator.add("./predictive-search-tree-db/src/main/resources/csvFilesNew/hotels.w2m.2019-03-26T23_00_01.csv");
 
+            int fileNumber = 1;
+
             for (final String file : filesWithDefaultSeparator) {
-                processFile(file, fileWriter, false);
-                fileWriter.write(",");
+                processFile(file, false, String.valueOf(fileNumber));
+                fileNumber++;
             }
 
-            processFile(filesWithSpecialSeparator.get(0), fileWriter, true);
-            fileWriter.write(",");
-
-            processFile(filesWithSpecialSeparator.get(1), fileWriter, true);
-            fileWriter.write(";\nEND");
-            fileWriter.flush();
+            for (final String file : filesWithSpecialSeparator) {
+                processFile(file, true, String.valueOf(fileNumber));
+                fileNumber++;
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     private static void processFile(final String filePath,
-                                    final FileWriter fileWriter,
-                                    final boolean specialSeparator) throws IOException {
+                                    final boolean specialSeparator,
+                                    final String newFileNumber) throws IOException {
+        final File newFile = new File("./predictive-search-tree-db/src/main/resources/sql/hotels" + newFileNumber + ".sql");
+        newFile.createNewFile();
+
+        final FileWriter fileWriter = new FileWriter(newFile, false);
+        fileWriter.write("INSERT INTO hotels(ids, star, name) values");
+
         final CSVReader reader = specialSeparator ?
                 new CSVReader(new FileReader(filePath), ';') : new CSVReader(new FileReader(filePath));
+
         String[] values = reader.readNext();
         writeFile(fileWriter, values);
         while ((values = reader.readNext()) != null) {
             fileWriter.write(",");
             writeFile(fileWriter, values);
         }
+
+        fileWriter.write(";");
+        fileWriter.flush();
     }
 
     private static void writeFile(final FileWriter fileWriter,
                                   final String[] values) throws IOException {
+        values[15] = values[15].replaceAll("\"", "");
         fileWriter.write("\n(\"" + values[0] + "\", \"" + values[4] + "\", \"" + values[15] + "\")");
     }
 }
