c4877d367c2b53f02ebfc886e67b4353365caa09
MariaRozhkova <mary.wootsen.96@gmail.com>
2019-04-05 10:40:09 +0300

    from csv to sql

--- predictive-search-tree-db/src/main/java/com/hematite/predictive/search/dataprovider/repository/HotelRepository.java
+++ predictive-search-tree-db/src/main/java/com/hematite/predictive/search/dataprovider/repository/HotelRepository.java
@@ -1,10 +1,17 @@
 package com.hematite.predictive.search.dataprovider.repository;
 
 import com.hematite.predictive.search.dataprovider.entity.HotelEntity;
+import com.hematite.predictive.search.tree.NodeData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface HotelRepository extends JpaRepository<HotelEntity, Long> {
+
+    List<HotelEntity> findByName(final String name);
 
 }
---
+++ predictive-search-tree-rest/src/test/java/com/hematite/predictive/search/rest/TreeGenerator.java
@@ -0,0 +1,79 @@
+package com.hematite.predictive.search.rest;
+
+import com.hematite.predictive.search.dataprovider.converter.HotelConverter;
+import com.hematite.predictive.search.dataprovider.entity.HotelEntity;
+import com.hematite.predictive.search.dataprovider.repository.HotelRepository;
+import com.hematite.predictive.search.factory.PredictiveSearchTreeFactory;
+import com.hematite.predictive.search.tree.NodeData;
+import com.hematite.predictive.search.tree.TreeNode;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class TreeGenerator {
+
+    private final PredictiveSearchTreeFactory predictiveSearchTreeFactory = new PredictiveSearchTreeFactory();
+
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    @Test
+    public void testFindInTree() {
+
+        final TreeNode treeNode = predictiveSearchTreeFactory.createTree(HotelConverter.convertToDto(hotelRepository.findAll()));
+
+        final Instant start = Instant.now();
+
+        try {
+            final List<String> lines = Files.readAllLines(Paths.get(ClassLoader.getSystemResource("requests.txt").toURI()));
+            for (String value : lines) {
+                treeNode.search(value);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        final Instant finish = Instant.now();
+        final long timeElapsed = Duration.between(start, finish).toMillis();
+
+    }
+
+    @Test
+    public void testFindInDatabase() {
+
+        final Instant start = Instant.now();
+
+        try {
+            final List<String> lines = Files.readAllLines(Paths.get(ClassLoader.getSystemResource("requests.txt").toURI()));
+            for (String value : lines) {
+                hotelRepository.findByName(value);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        final Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toMillis();
+
+    }
+
+}
