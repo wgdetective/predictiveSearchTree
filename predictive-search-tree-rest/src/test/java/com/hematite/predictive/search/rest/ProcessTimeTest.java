0a6306bbd5e1ef46ae2a74dcefa7608df40abd9f
dePelimesha <walik-1998@mail.ru>
2019-04-15 15:54:02 +0300

    refactor performance test

--- predictive-search-tree-rest/src/test/java/com/hematite/predictive/search/rest/ProcessTimeTest.java
+++ predictive-search-tree-rest/src/test/java/com/hematite/predictive/search/rest/ProcessTimeTest.java
@@ -4,6 +4,9 @@ import com.hematite.predictive.search.dataprovider.converter.HotelConverter;
 import com.hematite.predictive.search.dataprovider.entity.HotelEntity;
 import com.hematite.predictive.search.dataprovider.repository.HotelRepository;
 import com.hematite.predictive.search.factory.PredictiveSearchTreeFactory;
+import com.hematite.predictive.search.neo4j.PredictiveSearchTreeNeoFactory;
+import com.hematite.predictive.search.neo4j.entity.NodeDataNeo;
+import com.hematite.predictive.search.neo4j.repository.HotelNeo4JRepository;
 import com.hematite.predictive.search.tree.NodeData;
 import com.hematite.predictive.search.tree.TreeNode;
 import org.junit.Test;
@@ -11,8 +14,12 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.neo4j.AutoConfigureDataNeo4j;
+import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
@@ -21,10 +28,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureDataNeo4j
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class ProcessTimeTest {
 
@@ -34,6 +43,9 @@ public class ProcessTimeTest {
 
     @Autowired
     private HotelRepository hotelRepository;
+
+    @Autowired
+    private HotelNeo4JRepository hotelNeo4JRepository;
 
     @Test
     public void testFindInTree() {
@@ -58,6 +70,35 @@ public class ProcessTimeTest {
         } catch (final IOException | URISyntaxException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void testFindInNeo4J() throws URISyntaxException, IOException {
+        final PredictiveSearchTreeNeoFactory predictiveSearchTreeNeoFactory = new PredictiveSearchTreeNeoFactory(hotelNeo4JRepository);
+        final List<String> nodes = Files.readAllLines(Paths.get(ClassLoader.getSystemResource("hotels.txt").toURI()));
+
+        final List<NodeDataNeo> nodeDataNeoList = new ArrayList<>();
+        for (final String nodeData : nodes) {
+            final NodeDataNeo nodeDataNeo = new NodeDataNeo();
+            nodeDataNeo.setKey(nodeData.toLowerCase());
+            nodeDataNeo.setData(nodeData);
+            nodeDataNeoList.add(nodeDataNeo);
+        }
+
+        final Instant startTree = Instant.now();
+        predictiveSearchTreeNeoFactory.createTree(nodeDataNeoList);
+        final Instant finishTree = Instant.now();
+        final long timeElapsedTree = Duration.between(startTree, finishTree).toMillis();
+        LOGGER.info("Time for create tree with Neo4J: {}", timeElapsedTree);
+
+        final List<String> lines = Files.readAllLines(Paths.get(ClassLoader.getSystemResource("hotelsQuery.txt").toURI()));
+        final Instant start = Instant.now();
+        for (final String value : lines) {
+            hotelNeo4JRepository.findByKey(value);
+        }
+        final Instant finish = Instant.now();
+        final long timeElapsed = Duration.between(start, finish).toMillis();
+        LOGGER.info("Time for processing queries with tree: {}", timeElapsed);
     }
 
     @Test
